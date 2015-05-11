6adf0955f4cdc693364afb83f2258211ad140e4d
jpadolf <jpadolf@gmail.com>
2015-05-10 23:03:54 -0500

    Pedestrian Detection Code - 1.01
    Minor filename changes for clarity.

--- make_video.m
+++ make_video.m
@@ -1,29 +1,29 @@
 %% Create Input Video
-fnames = dir('../VideoImages/*.pgm');
+fnames = dir('../Files/VideoImages/*.pgm');
 numfids = length(fnames);
 
-outputVideo = VideoWriter(fullfile('../Videos','input_video.avi'));
+outputVideo = VideoWriter(fullfile('../Files/Videos','input_video.avi'));
 outputVideo.FrameRate = 13;
 open(outputVideo);
 
 for i = 1:numfids
-    img = imread(fullfile('../VideoImages',fnames(i).name));
+    img = imread(fullfile('../Files/VideoImages',fnames(i).name));
     writeVideo(outputVideo,img);
 end
 
 close(outputVideo);
 
 %% Create Output Video
-fnames = dir('../VideoImages/*.pgm');
+fnames = dir('../Files/VideoImages/*.pgm');
 numfids = length(fnames);
 
-outputVideo = VideoWriter(fullfile('../Videos','output_video.avi'));
+outputVideo = VideoWriter(fullfile('../Files/Videos','output_video.avi'));
 outputVideo.FrameRate = 13;
 open(outputVideo);
 for i = 1:numfids
     tic
     i
-    NMSwindows = detect_pedestrian(fullfile('../VideoImages',fnames(i).name),mod2_csvm,0,16,.75,0);
+    NMSwindows = detect_pedestrian(fullfile('../Files/VideoImages',fnames(i).name),mod2_csvm,0,16,.75,0);
     frame = getframe;
     writeVideo(outputVideo,frame);
     toc
--- startup.m
+++ startup.m
@@ -1,2 +1,11 @@
-load '../newtrainmod2_laptop_csvm.mat';
-run('../vlfeat-0.9.20/toolbox/vl_setup');
+% startup.m loads the classification model and sets up the VLFeat library 
+% 
+% Change the current directory to the "Code" folder containing this file
+% and all other MATLAB functions.
+% 
+% The parent folder of "Code" should also include the "Files" folder,
+% which contains the model, the VLFeat library, and all testing images.
+
+clear; clc;
+load '../Files/model.mat';
+run('../Files/vlfeat-0.9.20/toolbox/vl_setup');
